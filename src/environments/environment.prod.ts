2255c5b86ea77001537c43fc49efd0c6cac4e8c0
Ara Winters <ara.winters@gmail.com>
2020-02-10 17:01:06 -0800

    WIP adjustments for docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,3 +10,7 @@ export const apiConfig: SzRestConfigurationParameters = {
   'basePath': '/api',
   'withCredentials': true
 };
+
+/** security options for admin area */
+import * as SEC_OPTS from '../../auth/auth.conf.json';
+export const securityConfig = SEC_OPTS;
