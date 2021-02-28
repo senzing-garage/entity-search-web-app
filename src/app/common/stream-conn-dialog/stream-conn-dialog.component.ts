d05d59f065da25a865a2141df7fa85af1f9e7a39
Ara Winters <ara.winters@gmail.com>
2021-02-27 23:22:48 -0800

    #146 WIP added GUI controls for new admin stream analyze/load options

--- src/app/common/stream-conn-dialog/stream-conn-dialog.component.ts
+++ src/app/common/stream-conn-dialog/stream-conn-dialog.component.ts
@@ -1,22 +1,45 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { WebSocketService } from '../../services/websocket.service';
 import { AdminBulkDataService } from '../../services/admin.bulk-data.service';
 
 //import { AdminStreamConnProperties } from '../../services/admin.bulk-data.service';
 import { AdminStreamConnProperties, AdminStreamAnalysisConfig, AdminStreamLoadConfig } from '@senzing/sdk-components-ng';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'stream-conn-dialog',
     templateUrl: 'stream-conn-dialog.component.html',
     styleUrls: ['stream-conn-dialog.component.scss']
   })
-  export class AdminStreamConnDialogComponent {
+  export class AdminStreamConnDialogComponent implements OnInit, OnDestroy {
+    /** subscription to notify subscribers to unbind */
+    public unsubscribe$ = new Subject<void>();
+    
     public set streamHost(value: string) {
       this.data.streamConnectionProperties.hostname = value;
     }
     public get streamHost() {
       return this.data.streamConnectionProperties.hostname;
+    }
+    public set streamPort(value: number) {
+      this.data.streamConnectionProperties.port = value;
+    }
+    public get streamPort() {
+      return this.data.streamConnectionProperties.port;
+    }
+    public get streamReconnect(): boolean {
+      return this.data.streamConnectionProperties.reconnectOnClose;
+    }
+    public set streamReconnect(value: boolean) {
+      this.data.streamConnectionProperties.reconnectOnClose = value;
+    }
+    public get streamReconnectionAttempts() {
+      return (this.data && this.data.streamConnectionProperties && this.data.streamConnectionProperties.reconnectConsecutiveAttemptLimit) ? this.data.streamConnectionProperties.reconnectConsecutiveAttemptLimit : -2;
+    }
+    public set streamReconnectionAttempts(value: number) {
+      this.data.streamConnectionProperties.reconnectConsecutiveAttemptLimit = value;
     }
     public get wsUUID(): string {
       return this.data.streamConnectionProperties.clientId;
@@ -34,8 +57,29 @@ import { AdminStreamConnProperties, AdminStreamAnalysisConfig, AdminStreamLoadCo
       return (this.data && this.data.streamConnectionProperties && this.data.streamConnectionProperties.connectionTest) ?  this.data.streamConnectionProperties.connectionTest : false;
     }
 
+    public get autoCreateMissingDataSources(): boolean {
+      return (this.data && this.data.streamLoadConfig && this.data.streamLoadConfig.autoCreateMissingDataSources) ? true : false;
+    }
+    public set autoCreateMissingDataSources(value: boolean) {
+      this.data.streamLoadConfig.autoCreateMissingDataSources = value;
+    }
+    public get mapEmptyDataSourcesTo(): string {
+      return (this.data && this.data.streamLoadConfig && this.data.streamLoadConfig.assignMissingDataSourceRecordsToStaticTarget) ? (this.data.streamLoadConfig.assignMissingDataSourceRecordsToStaticTarget as string) : "";
+    }
+    public set mapEmptyDataSourcesTo(value: string) {
+      this.data.streamLoadConfig.assignMissingDataSourceRecordsToStaticTarget = value;
+    }
+    private _mapEmptyDataSourcesToValue = false;
+    public get mapEmptyDataSourcesToValue(): boolean {
+      return this._mapEmptyDataSourcesToValue;
+    }
+    public set mapEmptyDataSourcesToValue(value: boolean) {
+      this._mapEmptyDataSourcesToValue = value;
+    }
+
     public wsAnalysisSampleSizes = [
       100,
+      500,
       1000,
       5000,
       10000,
@@ -43,8 +87,19 @@ import { AdminStreamConnProperties, AdminStreamAnalysisConfig, AdminStreamLoadCo
       50000,
       100000
     ];
+    public wsReconnectionAttemptsOptions = [
+      {value: -2, text: 'unlimited'},
+      {value: -1, text: 'none'},
+      {value: 1, text: '1'},
+      {value: 2, text: '2'},
+      {value: 3, text: '3'},
+      {value: 5, text: '5'},
+      {value: 10, text: '10'},
+      {value: 20, text: '20'}
+    ]
 
     public testStatus = "";
+    public isTesting  = false;
 
     constructor(
       public dialogRef: MatDialogRef<AdminStreamConnDialogComponent>,
@@ -86,6 +141,28 @@ import { AdminStreamConnProperties, AdminStreamAnalysisConfig, AdminStreamLoadCo
         this.data.streamConnectionProperties.connectionTest = false;
       }
     }
+    /**
+     * listen for websocket service errors
+     */
+    ngOnInit() {
+      this.webSocketService.onError.pipe(
+        takeUntil(this.unsubscribe$)
+      ).subscribe((error: Error) => {
+        console.warn('error');
+        if(this.isTesting) {
+          this.isTesting  = false;
+          this.testStatus = 'Test failed';
+        }
+      });
+    }
+
+    /**
+     * unsubscribe event streams
+     */
+    ngOnDestroy() {
+      this.unsubscribe$.next();
+      this.unsubscribe$.complete();
+    }
   
     onNoClick(): void {
       this.dialogRef.close();
@@ -93,20 +170,30 @@ import { AdminStreamConnProperties, AdminStreamAnalysisConfig, AdminStreamLoadCo
  
     public testConnection(event: Event) {
       this.testStatus = "Opening Connection.."
+      this.isTesting  = true;
       this.webSocketService.testConnection(this.data.streamConnectionProperties).subscribe((isValid: boolean) => {
         if(isValid) {
           this.data.streamConnectionProperties.connectionTest = true;
+          this.isTesting = false;
           this.testStatus = "Connection Opened Successfully"
           setTimeout(() => {
             // hide status message
             this.testStatus = "Closing Connection.."
             setTimeout(() => {
               this.testStatus = undefined;
+              this.isTesting = false;
             }, 2000);
           }, 10000)
         } else {
           this.data.streamConnectionProperties.connectionTest = false;
+          this.isTesting = false;
         }
+      }, (error: Error) => {
+        console.warn('error');
+        this.isTesting = false;
       })
+    }
+    public abortTest(event: Event) {
+      this.isTesting = false;
     }
   }
