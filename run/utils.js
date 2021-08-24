58e6e558579899f185f5a4ef078f6956d7fa7d64
Ara Winters <arawinters@users.noreply.github.com>
2021-08-24 08:07:42 -0700

    Issue183.awinters.1 (#184)
    #183 startup issue fix

---
+++ run/utils.js
@@ -0,0 +1,78 @@
+
+let getHostnameFromUrl = function(url) {
+    if(!url) return;
+    if(url) {
+        var hostname  = url;
+        if(hostname.indexOf && hostname.indexOf('://') > -1) {
+            // strip protocol off
+            let urlTokened = hostname.split('://');
+            hostname  = urlTokened[1];
+        }
+        if(hostname.indexOf && hostname.indexOf(':') > -1) {
+            // strip port off
+            _ntemp = hostname.split(':')[0];
+            hostname = _ntemp;
+        }
+        if(hostname.indexOf && hostname.indexOf('/') > -1){
+            // strip off anything in path
+            _ntemp = hostname.split('/')[0];
+            hostname = _ntemp;
+        }
+        //console.log(`set hostname to: "${hostname}"`);
+
+        return hostname;
+    }
+    return;
+}
+
+let getPortFromUrl  = function(url) {
+    if(!url) return;
+    if(url) {
+        var hostname    = url;
+        var portnumber  = 8250;
+        if(hostname.indexOf('://') > -1) {
+            // strip protocol off
+            let urlTokened = hostname.split('://');
+            hostname  = urlTokened[1];
+        }
+        if(hostname.indexOf(':') > -1) {
+            // keep port
+            let _ntemp = hostname.split(':');
+            if(_ntemp.length > 1 && _ntemp[1]) {
+                portnumber    = parseInt(_ntemp[1]);
+            }
+        }
+        return portnumber;
+    }
+}
+
+let getProtocolFromUrl = function(url) {
+    if(!url) return;
+    if( url ) {
+        let protocol    = 'http';
+        if(url.indexOf && url.indexOf('://') > -1) {
+            let urlTokened = url.split('://');
+            protocol  = urlTokened[0];
+        }
+        return protocol;
+    }
+}
+
+let replaceProtocol = function(protoStr, url) {
+    if(!url) return;
+    if( url ) {
+        if(url.indexOf && url.indexOf('://') > -1) {
+            let urlTokened = url.split('://');
+            urlTokened[0] = protoStr;
+            url = urlTokened.join('://');
+        }
+    }
+    return url;
+}
+
+module.exports = {
+    "getHostnameFromUrl": getHostnameFromUrl,
+    "getPortFromUrl": getPortFromUrl,
+    "getProtocolFromUrl": getProtocolFromUrl,
+    "replaceProtocol": replaceProtocol
+}
