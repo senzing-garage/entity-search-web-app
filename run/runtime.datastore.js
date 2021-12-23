2ebb8844ea1d285f15efe9c015ae5cc99ef7e128
Ara <ara.winters@gmail.com>
2021-12-23 12:58:39 -0800

    #235 debug

--- run/runtime.datastore.js
+++ run/runtime.datastore.js
@@ -318,22 +318,26 @@ class inMemoryConfig extends EventEmitter {
           // poc server supports adding datasources and importing data
           this.streamServerConfiguration = undefined;
           this.webConfiguration.streamLoading = false;
+          console.log('--------------------------  onApiServerReady: stream loading disabled #1 --------------------------');
           this.emit('streamLoadingChanged',this.webConfiguration.streamLoading);
         }
         if(!serverInfo.data.loadQueueConfigured) {
           // poc server does not support loading through stream socket
           this.streamServerConfiguration = undefined;
           this.webConfiguration.streamLoading = false;
+          console.log('--------------------------  onApiServerReady: stream loading disabled #2 --------------------------');
           this.emit('streamLoadingChanged',this.webConfiguration.streamLoading);
         }
       } else if(serverInfo.data && !serverInfo.data.adminEnabled) {
         // standard rest server that supports loading data
         this.streamServerConfiguration = undefined;
         this.webConfiguration.streamLoading = false;
+        console.log('--------------------------  onApiServerReady: stream loading disabled #3 --------------------------');
         this.emit('streamLoadingChanged',this.webConfiguration.streamLoading);
       }
     } else {
       this.webConfiguration.streamLoading = false;
+      console.log('--------------------------  onApiServerReady: stream loading disabled #4 --------------------------');
       this.emit('streamLoadingChanged',this.webConfiguration.streamLoading);
     }
     // now notify any listeners that we fully have the data we need
